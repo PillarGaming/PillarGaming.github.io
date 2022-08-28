a37bebec303371f47051ac65567496955634733c
PillarGaming <108586267+PillarGaming@users.noreply.github.com>
2022-08-28 19:25:31 +0300

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,968 @@
+var simplemaps_worldmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "http://simplemaps.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AF: {
+      name: "Afganistanas"
+    },
+    AO: {
+      name: "Angola"
+    },
+    AL: {
+      name: "Albanija"
+    },
+    AE: {
+      name: "Jungtiniai Arabų Emyratai"
+    },
+    AR: {
+      name: "Argentina"
+    },
+    AM: {
+      name: "Armenija"
+    },
+    AU: {
+      name: "Australija"
+    },
+    AT: {
+      name: "Austrija"
+    },
+    AZ: {
+      name: "Azerbaidžanas"
+    },
+    BI: {
+      name: "Burundis"
+    },
+    BE: {
+      name: "Belgija"
+    },
+    BJ: {
+      name: "Beninas"
+    },
+    BF: {
+      name: "Burkina Fasas"
+    },
+    BD: {
+      name: "Bangladešas"
+    },
+    BG: {
+      name: "Bulgarija"
+    },
+    BH: {
+      name: "Bahreinas"
+    },
+    BA: {
+      name: "Bosnija ir Hercegovina"
+    },
+    BY: {
+      name: "Belarusija"
+    },
+    BZ: {
+      name: "Belize"
+    },
+    BO: {
+      name: "Bolivija"
+    },
+    BR: {
+      name: "Brazilija"
+    },
+    BN: {
+      name: "Brunėjus"
+    },
+    BT: {
+      name: "Butanas"
+    },
+    BW: {
+      name: "Botsvana"
+    },
+    CF: {
+      name: "Centrinės Afrikos Republika"
+    },
+    CA: {
+      name: "Kanada"
+    },
+    CH: {
+      name: "Šveicarija"
+    },
+    CL: {
+      name: "Čilė"
+    },
+    CN: {
+      name: "Kinija"
+    },
+    CI: {
+      name: "Côte d'Ivoire"
+    },
+    CM: {
+      name: "Kamerūnas"
+    },
+    CD: {
+      name: "Demokratine Kongo Respublika"
+    },
+    CG: {
+      name: "Kongo Respublika"
+    },
+    CO: {
+      name: "Kolumbija"
+    },
+    CR: {
+      name: "Costa Rica"
+    },
+    CU: {
+      name: "Kuba"
+    },
+    CZ: {
+      name: "Čekija"
+    },
+    DE: {
+      name: "Vokietija"
+    },
+    DJ: {
+      name: "Džibutis"
+    },
+    DK: {
+      name: "Danija"
+    },
+    DO: {
+      name: "Dominikos Respublika"
+    },
+    DZ: {
+      name: "Alžyras"
+    },
+    EC: {
+      name: "Ekvadoras"
+    },
+    EG: {
+      name: "Egiptas"
+    },
+    ER: {
+      name: "Eritrėja"
+    },
+    EE: {
+      name: "Estija"
+    },
+    ET: {
+      name: "Etiopija"
+    },
+    FI: {
+      name: "Suomija"
+    },
+    FJ: {
+      name: "Fidzi"
+    },
+    GA: {
+      name: "Gabonas"
+    },
+    GB: {
+      name: "Jungtinė Karalystė"
+    },
+    GE: {
+      name: "Džordžija"
+    },
+    GH: {
+      name: "Gana"
+    },
+    GN: {
+      name: "Gvinėja"
+    },
+    GM: {
+      name: "Gambija"
+    },
+    GW: {
+      name: "Bisau Gvinėja"
+    },
+    GQ: {
+      name: "Ekvatoriaus Gvinėja"
+    },
+    GR: {
+      name: "Graikija"
+    },
+    GL: {
+      name: "Grenlandija"
+    },
+    GT: {
+      name: "Gvatemala"
+    },
+    GY: {
+      name: "Gujana"
+    },
+    HN: {
+      name: "Hondūras"
+    },
+    HR: {
+      name: "Kroatija"
+    },
+    HT: {
+      name: "Haitis"
+    },
+    HU: {
+      name: "Vengrija"
+    },
+    ID: {
+      name: "Indonezija"
+    },
+    IN: {
+      name: "Indija"
+    },
+    IE: {
+      name: "Airija"
+    },
+    IR: {
+      name: "Iranas"
+    },
+    IQ: {
+      name: "Irakas"
+    },
+    IS: {
+      name: "Islandija"
+    },
+    IL: {
+      name: "Izraelis"
+    },
+    IT: {
+      name: "Italija"
+    },
+    JM: {
+      name: "Jamaika"
+    },
+    JO: {
+      name: "Jordanija"
+    },
+    JP: {
+      name: "Japanonija",
+      color: "#ff0000"
+    },
+    KZ: {
+      name: "Kazakhstan"
+    },
+    KE: {
+      name: "Kenya"
+    },
+    KG: {
+      name: "Kyrgyzstan"
+    },
+    KH: {
+      name: "Cambodia"
+    },
+    KR: {
+      name: "Republic of Korea"
+    },
+    XK: {
+      name: "Kosovo"
+    },
+    KW: {
+      name: "Kuwait"
+    },
+    LA: {
+      name: "Lao PDR"
+    },
+    LB: {
+      name: "Lebanon"
+    },
+    LR: {
+      name: "Liberia"
+    },
+    LY: {
+      name: "Libya"
+    },
+    LK: {
+      name: "Sri Lanka"
+    },
+    LS: {
+      name: "Lesotho"
+    },
+    LT: {
+      name: "Lithuania"
+    },
+    LU: {
+      name: "Luxembourg"
+    },
+    LV: {
+      name: "Latvia"
+    },
+    MA: {
+      name: "Morocco"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    MG: {
+      name: "Madagascar"
+    },
+    MX: {
+      name: "Mexico"
+    },
+    MK: {
+      name: "Macedonia"
+    },
+    ML: {
+      name: "Mali"
+    },
+    MM: {
+      name: "Myanmar"
+    },
+    ME: {
+      name: "Montenegro"
+    },
+    MN: {
+      name: "Mongolia"
+    },
+    MZ: {
+      name: "Mozambique"
+    },
+    MR: {
+      name: "Mauritania"
+    },
+    MW: {
+      name: "Malawi"
+    },
+    MY: {
+      name: "Malaysia"
+    },
+    NA: {
+      name: "Namibia"
+    },
+    NE: {
+      name: "Niger"
+    },
+    NG: {
+      name: "Nigeria"
+    },
+    NI: {
+      name: "Nicaragua"
+    },
+    NL: {
+      name: "Netherlands"
+    },
+    NO: {
+      name: "Norway"
+    },
+    NP: {
+      name: "Nepal"
+    },
+    NZ: {
+      name: "New Zealand"
+    },
+    OM: {
+      name: "Oman"
+    },
+    PK: {
+      name: "Pakistan"
+    },
+    PA: {
+      name: "Panama"
+    },
+    PE: {
+      name: "Peru"
+    },
+    PH: {
+      name: "Philippines"
+    },
+    PG: {
+      name: "Papua New Guinea"
+    },
+    PL: {
+      name: "Poland"
+    },
+    KP: {
+      name: "Dem. Rep. Korea"
+    },
+    PT: {
+      name: "Portugal"
+    },
+    PY: {
+      name: "Paraguay"
+    },
+    PS: {
+      name: "Palestine"
+    },
+    QA: {
+      name: "Qatar"
+    },
+    RO: {
+      name: "Romania"
+    },
+    RU: {
+      name: "Russia"
+    },
+    RW: {
+      name: "Rwanda"
+    },
+    EH: {
+      name: "Western Sahara"
+    },
+    SA: {
+      name: "Saudi Arabia"
+    },
+    SD: {
+      name: "Sudan"
+    },
+    SS: {
+      name: "South Sudan"
+    },
+    SN: {
+      name: "Senegal"
+    },
+    SL: {
+      name: "Sierra Leone"
+    },
+    SV: {
+      name: "El Salvador"
+    },
+    RS: {
+      name: "Serbia"
+    },
+    SR: {
+      name: "Suriname"
+    },
+    SK: {
+      name: "Slovakia"
+    },
+    SI: {
+      name: "Slovenia"
+    },
+    SE: {
+      name: "Sweden"
+    },
+    SZ: {
+      name: "Swaziland"
+    },
+    SY: {
+      name: "Syria"
+    },
+    TD: {
+      name: "Chad"
+    },
+    TG: {
+      name: "Togo"
+    },
+    TH: {
+      name: "Thailand"
+    },
+    TJ: {
+      name: "Tajikistan"
+    },
+    TM: {
+      name: "Turkmenistan"
+    },
+    TL: {
+      name: "Timor-Leste"
+    },
+    TN: {
+      name: "Tunisia"
+    },
+    TR: {
+      name: "Turkey"
+    },
+    TW: {
+      name: "Taiwan"
+    },
+    TZ: {
+      name: "Tanzania"
+    },
+    UG: {
+      name: "Uganda"
+    },
+    UA: {
+      name: "Ukraine"
+    },
+    UY: {
+      name: "Uruguay"
+    },
+    US: {
+      name: "United States"
+    },
+    UZ: {
+      name: "Uzbekistan"
+    },
+    VE: {
+      name: "Venezuela"
+    },
+    VN: {
+      name: "Vietnam"
+    },
+    VU: {
+      name: "Vanuatu"
+    },
+    YE: {
+      name: "Yemen"
+    },
+    ZA: {
+      name: "South Africa"
+    },
+    ZM: {
+      name: "Zambia"
+    },
+    ZW: {
+      name: "Zimbabwe"
+    },
+    SO: {
+      name: "Somalia"
+    },
+    GF: {
+      name: "France"
+    },
+    FR: {
+      name: "France"
+    },
+    ES: {
+      name: "Spain"
+    },
+    AW: {
+      name: "Aruba"
+    },
+    AI: {
+      name: "Anguilla"
+    },
+    AD: {
+      name: "Andorra"
+    },
+    AG: {
+      name: "Antigua and Barbuda"
+    },
+    BS: {
+      name: "Bahamas"
+    },
+    BM: {
+      name: "Bermuda"
+    },
+    BB: {
+      name: "Barbados"
+    },
+    KM: {
+      name: "Comoros"
+    },
+    CV: {
+      name: "Cape Verde"
+    },
+    KY: {
+      name: "Cayman Islands"
+    },
+    DM: {
+      name: "Dominica"
+    },
+    FK: {
+      name: "Falkland Islands"
+    },
+    FO: {
+      name: "Faeroe Islands"
+    },
+    GD: {
+      name: "Grenada"
+    },
+    HK: {
+      name: "Hong Kong"
+    },
+    KN: {
+      name: "Saint Kitts and Nevis"
+    },
+    LC: {
+      name: "Saint Lucia"
+    },
+    LI: {
+      name: "Liechtenstein"
+    },
+    MF: {
+      name: "Saint Martin (French)"
+    },
+    MV: {
+      name: "Maldives"
+    },
+    MT: {
+      name: "Malta"
+    },
+    MS: {
+      name: "Montserrat"
+    },
+    MU: {
+      name: "Mauritius"
+    },
+    NC: {
+      name: "New Caledonia"
+    },
+    NR: {
+      name: "Nauru"
+    },
+    PN: {
+      name: "Pitcairn Islands"
+    },
+    PR: {
+      name: "Puerto Rico"
+    },
+    PF: {
+      name: "French Polynesia"
+    },
+    SG: {
+      name: "Singapore"
+    },
+    SB: {
+      name: "Solomon Islands"
+    },
+    ST: {
+      name: "São Tomé and Principe"
+    },
+    SX: {
+      name: "Saint Martin (Dutch)"
+    },
+    SC: {
+      name: "Seychelles"
+    },
+    TC: {
+      name: "Turks and Caicos Islands"
+    },
+    TO: {
+      name: "Tonga"
+    },
+    TT: {
+      name: "Trinidad and Tobago"
+    },
+    VC: {
+      name: "Saint Vincent and the Grenadines"
+    },
+    VG: {
+      name: "British Virgin Islands"
+    },
+    VI: {
+      name: "United States Virgin Islands"
+    },
+    CY: {
+      name: "Cyprus"
+    },
+    RE: {
+      name: "Reunion (France)"
+    },
+    YT: {
+      name: "Mayotte (France)"
+    },
+    MQ: {
+      name: "Martinique (France)"
+    },
+    GP: {
+      name: "Guadeloupe (France)"
+    },
+    CW: {
+      name: "Curaco (Netherlands)"
+    },
+    IC: {
+      name: "Canary Islands (Spain)"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "36.2048",
+      lng: "138.2529",
+      name: "Japonija",
+      url: "https://halijas420.wixsite.com/my-site/post/____a-1",
+      image_url: "https://cdn.discordapp.com/attachments/505726033819860992/1013430891583905842/shibuya-crossing-in-japan-at-night.jpg",
+      description: "Paskausk kad skaityti toliau"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {
+    "0": {
+      name: "Šiaures Amerika",
+      states: [
+        "MX",
+        "CA",
+        "US",
+        "GL",
+        "BM"
+      ]
+    },
+    "1": {
+      name: "Pietų Amerika",
+      states: [
+        "EC",
+        "AR",
+        "VE",
+        "BR",
+        "CO",
+        "BO",
+        "PE",
+        "BZ",
+        "CL",
+        "CR",
+        "CU",
+        "DO",
+        "SV",
+        "GT",
+        "GY",
+        "GF",
+        "HN",
+        "NI",
+        "PA",
+        "PY",
+        "PR",
+        "SR",
+        "UY",
+        "JM",
+        "HT",
+        "BS",
+        "FK",
+        "AI",
+        "AG",
+        "AW",
+        "BB",
+        "VG",
+        "KY",
+        "DM",
+        "MQ",
+        "LC",
+        "VC",
+        "GD",
+        "GP",
+        "MS",
+        "TC",
+        "SX",
+        "MF",
+        "KN",
+        "CW"
+      ]
+    },
+    "2": {
+      name: "Europa",
+      states: [
+        "IT",
+        "NL",
+        "NO",
+        "DK",
+        "IE",
+        "GB",
+        "RO",
+        "DE",
+        "FR",
+        "AL",
+        "AM",
+        "AT",
+        "BY",
+        "BE",
+        "LU",
+        "BG",
+        "CZ",
+        "EE",
+        "GE",
+        "GR",
+        "HU",
+        "IS",
+        "LV",
+        "LT",
+        "MD",
+        "PL",
+        "PT",
+        "RS",
+        "SI",
+        "HR",
+        "BA",
+        "ME",
+        "MK",
+        "SK",
+        "ES",
+        "FI",
+        "SE",
+        "CH",
+        "TR",
+        "CY",
+        "UA",
+        "XK",
+        "MT",
+        "FO"
+      ]
+    },
+    "3": {
+      name: "Afrika ir Artimieji Rytai",
+      states: [
+        "QA",
+        "BH",
+        "SA",
+        "AE",
+        "SY",
+        "OM",
+        "KW",
+        "PK",
+        "AZ",
+        "AF",
+        "IR",
+        "IQ",
+        "IL",
+        "PS",
+        "JO",
+        "LB",
+        "YE",
+        "TJ",
+        "TM",
+        "UZ",
+        "KG",
+        "NE",
+        "AO",
+        "EG",
+        "TN",
+        "GA",
+        "DZ",
+        "LY",
+        "CG",
+        "GQ",
+        "BJ",
+        "BW",
+        "BF",
+        "BI",
+        "CM",
+        "CF",
+        "TD",
+        "CI",
+        "CD",
+        "DJ",
+        "ET",
+        "GM",
+        "GH",
+        "GN",
+        "GW",
+        "KE",
+        "LS",
+        "LR",
+        "MG",
+        "MW",
+        "ML",
+        "MA",
+        "MR",
+        "MZ",
+        "NA",
+        "NG",
+        "ER",
+        "RW",
+        "SN",
+        "SL",
+        "SO",
+        "ZA",
+        "SD",
+        "SS",
+        "SZ",
+        "TZ",
+        "TG",
+        "UG",
+        "EH",
+        "ZM",
+        "ZW",
+        "RE",
+        "KM",
+        "SC",
+        "MU",
+        "CV",
+        "IC",
+        "ST",
+        "YT"
+      ]
+    },
+    "4": {
+      name: "Pietų Aziją",
+      states: [
+        "SG",
+        "TW",
+        "IN",
+        "AU",
+        "MY",
+        "NP",
+        "NZ",
+        "TH",
+        "BN",
+        "JP",
+        "VN",
+        "LK",
+        "SB",
+        "FJ",
+        "BD",
+        "BT",
+        "KH",
+        "LA",
+        "MM",
+        "KP",
+        "PG",
+        "PH",
+        "KR",
+        "ID",
+        "CN",
+        "MV",
+        "NC",
+        "VU",
+        "NR"
+      ]
+    },
+    "5": {
+      name: "Šiaures Azija",
+      states: [
+        "MN",
+        "RU",
+        "KZ"
+      ]
+    }
+  }
+};
